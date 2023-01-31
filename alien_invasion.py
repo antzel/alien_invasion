f75da33011d65bc206c0ba2bbc210110596038e4
antishoker <zelenskiy.anton@gmail.com>
2023-01-31 21:48:29 +0300

    Добавлены классы Ship и Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Класс управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen  = pygame.display.set_mode((1200, 800))
+        self.screen  = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -19,11 +26,15 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
 
 
-#if __name__ == '__alien_invasion__':
+if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
-ai = AlienInvasion()
-ai.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
